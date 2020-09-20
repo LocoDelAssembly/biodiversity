d7dceb78edf7109424833291b39fea3d4db0bc79
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-09-20 16:54:41 -0300

    Some tweaks

--- lib/biodiversity/parser/gnparser.rb
+++ lib/biodiversity/parser/gnparser.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'open3'
+require 'csv'
+
 module Biodiversity
   # Parser provides a namespace for functions to parse scientific names.
   module Parser
@@ -48,17 +51,11 @@ module Biodiversity
         @pid = Process.pid
       end
 
-      def init_gnparser; end
-
-      def sync(&block)
+      def sync
         @semaphore.synchronize do
           start_gnparser unless Process.pid == @pid
-          block.call
+          yield
         end
-      end
-
-      def clean_name(name)
-        name.gsub(/(\n|\r\n|\r)/, ' ')
       end
 
       def push(name)
@@ -69,16 +66,11 @@ module Biodiversity
         parse_output(@stdout.gets)
       end
 
-      # gnparser interface to JSON-formatted output
-      class Compact < self
-        def parse_output(output)
-          JSON.parse(output, symbolize_names: true)
-        end
-
-        def format
-          'compact'
-        end
+      def clean_name(name)
+        name.gsub(/(\n|\r\n|\r)/, ' ')
       end
+
+      def init_gnparser; end
 
       # gnparser interface to CSV-formatted output
       class Csv < self
@@ -112,6 +104,17 @@ module Biodiversity
 
         def get_csv_value(csv, field_name)
           csv[@csv_mapping[field_name]]
+        end
+      end
+
+      # gnparser interface to JSON-formatted output
+      class Compact < self
+        def parse_output(output)
+          JSON.parse(output, symbolize_names: true)
+        end
+
+        def format
+          'compact'
         end
       end
     end
